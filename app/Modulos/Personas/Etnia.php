c37c602b4e6cf4e0722d31b6fabfde708e477cc8
Jonathan Castro <jonatancastro1@gmail.com>
2016-09-21 08:41:48 -0500

    Arreglo trait inexistente

--- app/Modulos/Personas/Etnia.php
+++ app/Modulos/Personas/Etnia.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Modulos\Planilla\Traits\DocumentoContratistas as DocumentoContratistas;
 namespace App\Modulos\Personas;
 
 use Idrd\Usuarios\Repo\Etnia as MEtnia;
 
 class Etnia extends MEtnia
 {
-    use DocumentoContratistas;
+	
 }
